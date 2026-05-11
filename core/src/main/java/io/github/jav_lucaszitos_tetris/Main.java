9b39aac1b1673063b6d7cc0c7b77f276585b2936
lucas <lucasferreiraf2001@outlook.coim>
2026-05-11 20:17:27 -0300

    feat: refactor to matrix-based rendering and cleanup legacy assets

--- core/src/main/java/io/github/jav_lucaszitos_tetris/Main.java
+++ core/src/main/java/io/github/jav_lucaszitos_tetris/Main.java
@@ -1,75 +1,32 @@
 package io.github.jav_lucaszitos_tetris;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
+
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import java.awt.*;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    /**
-    private SpriteBatch batch;
-    private Texture image;
-     **/
-    Texture backgroundTexture;
-    Texture bucketTexture;
-    Texture dropTexture;
-    Sound dropSound;
-    Music music;
 
-    SpriteBatch spriteBatch;
     FitViewport viewport;
+    ShapeRenderer shapeRenderer;
+    int[][] background;
+    int[][] piece;
+    float pieceX = 0;
+    float pieceY = 0;
 
-    Sprite bucketSprite;
 
-    Vector2 touchPos;
-
-    Array<Sprite> dropSprites;
-
-    float dropTimer;
-
-    Rectangle bucketRectangle;
-    Rectangle dropRectangle;
 
     @Override
     public void create() {
-        /**
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-         **/
-        backgroundTexture = new Texture("background.png");
-        bucketTexture = new Texture("bucket.png");
-        dropTexture = new Texture("drop.png");
+        viewport = new FitViewport(800, 600);
+        shapeRenderer = new ShapeRenderer();
+        background = new int[20][10];
+        piece = new int[0][0];
 
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
-        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
-
-        spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
-
-        bucketSprite = new Sprite(bucketTexture);
-        bucketSprite.setSize(1, 1);
-        touchPos = new Vector2();
-
-        dropSprites = new Array<>();
-
-        bucketRectangle = new Rectangle();
-        dropRectangle = new Rectangle();
-
-        music.setLooping(true);
-        music.setVolume(.5f);
-        music.play();
     }
 
     @Override
@@ -77,101 +34,94 @@ public class Main extends ApplicationAdapter {
         viewport.update(width, height, true);
     }
 
+
     @Override
     public void render() {
-        input();
-        logic();
-        draw();
-    }
-
-    private void input() {
-        float speed = 4f;
-        float delta = Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
-            bucketSprite.translateX(speed * delta);
-        }else if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
-            bucketSprite.translateX(-speed * delta);
-        }
-
-        if(Gdx.input.isTouched()){
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(touchPos);
-            bucketSprite.setCenterX(touchPos.x);
-        }
-    }
-    private void logic() {
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-        float bucketWidth = bucketSprite.getWidth();
-        float bucketHeight = bucketSprite.getHeight();
-
-        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
-
-        float delta =  Gdx.graphics.getDeltaTime();
-
-        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
-
-        for (int i = dropSprites.size - 1; i >= 0; i--){
-            Sprite dropSprite = dropSprites.get(i);
-            float dropWidth = dropSprite.getWidth();
-            float dropHeight = dropSprite.getHeight();
-
-            dropSprite.translateY(-2f * delta);
-
-            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
-
-            if(dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
-            else if (bucketRectangle.overlaps(dropRectangle)) {
-                dropSprites.removeIndex(i);
-                dropSound.play();
-            }
-
-        }
-
-        dropTimer += delta;
-        if (dropTimer >= 1f) {
-            dropTimer = 0;
-            createDroplet();
-        }
-    }
-    private void draw() {
         ScreenUtils.clear(Color.BLACK);
-        viewport.apply();
-        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
-        spriteBatch.begin();
+        shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        scenario();
 
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
+        iTetris(zBlock, pieceX, pieceY);
+        shapeRenderer.end();
 
-        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
-        bucketSprite.draw(spriteBatch);
 
-        for(Sprite dropSprite : dropSprites){
-            dropSprite.draw(spriteBatch);
+    }
+
+    public void scenario(){
+        int block = 25;
+        float offsetX = 250;
+        float offsetY = 50;
+
+        for(int i = 0; i < background.length; i++) {
+            for(int j = 0; j < background[i].length; j++) {
+
+                float x = j * block;
+                float y = i * block;
+
+                shapeRenderer.setColor(Color.GRAY);
+                shapeRenderer.rect(offsetX + x, offsetY + y, block - 1, block - 1);
+            }
         }
-
-
-
-        spriteBatch.end();
     }
 
-    private void createDroplet(){
-        float dropWidth = 1;
-        float dropHeight = 1;
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-
-        Sprite dropSprite = new Sprite(dropTexture);
-        dropSprite.setSize(dropWidth, dropHeight);
-        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
-        dropSprite.setY(worldHeight);
-        dropSprites.add(dropSprite);
-
+    public void iTetris(int[][] piece, float pX, float pY) {
+        int block = 25;
+        for(int i = 0; i < piece.length; i++) {
+            for(int j = 0; j < piece[i].length; j++) {
+                if(piece[i][j] == 1) {
+                    float x = j * block;
+                    float y = i * block;
+                    float offsetX = 250;
+                    float offsetY = 50;
+                    shapeRenderer.setColor(Color.WHITE);
+                    shapeRenderer.rect(offsetX+x, offsetY+y, block - 1, block - 1);
+                }
+            }
+        }
     }
 
+    int[][] iBlock = {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0}
+    };
 
-    @Override
-    public void pause() {
-    }
+    int[][] tBlock = {
+        {0, 0, 0},
+        {1, 1, 1},
+        {0, 1, 0}
+    };
+
+    int[][] oBlock = {
+        {1, 1},
+        {1, 1}
+    };
+
+    int[][] lBlock = {
+        {0, 1, 0},
+        {0, 1, 0},
+        {0, 1, 1}
+    };
+
+    int[][] jBlock = {
+        {0, 1, 0},
+        {0, 1, 0},
+        {1, 1, 0}
+    };
+
+    int[][] sBlock = {
+        {0, 1, 1},
+        {1, 1, 0},
+        {0, 0, 0}
+    };
+
+    int[][] zBlock = {
+        {1, 1, 0},
+        {0, 1, 1},
+        {0, 0, 0}
+    };
+
 
 }
